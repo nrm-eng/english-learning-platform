ae3e83374372ba6415f469dcdc47f2829529d46a
NRM <roman.nikitchuk123@gmail.com>
2026-05-20 19:22:47 +0200

    feat: add application db context

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-namespace Infrastructure.Persistence
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    internal class ApplicationDbContext
+    public DbSet<User> Users { get; init; }
+    public DbSet<Role> Roles { get; init; }
+    public DbSet<Level> Levels { get; init; }
+    public DbSet<Exercise> Exercises { get; init; }
+    public DbSet<ExerciseType> ExerciseTypes { get; init; }
+    public DbSet<Question> Questions { get; init; }
+    public DbSet<AnswerOption> AnswerOptions { get; init; }
+    public DbSet<PlacementTest> PlacementTests { get; init; }
+    public DbSet<UserExerciseResult> UserExerciseResults { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.HasAnnotation("Relational:MigrationHistoryTable", "__efmigrationshistory");
+        base.OnModelCreating(modelBuilder);
     }
 }
