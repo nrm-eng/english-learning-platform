c191b085c66d92375a1db2d6993b750c1f411c20
NRM <roman.nikitchuk123@gmail.com>
2026-05-22 22:00:04 +0200

    refactor: fix migrations namespace and configure swagger launch settings

--- Infrastructure/Persistence/Migrations/20260522194231_InitialCreate.Designer.cs
+++ Infrastructure/Persistence/Migrations/20260522194231_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     [Migration("20260522194231_InitialCreate")]
