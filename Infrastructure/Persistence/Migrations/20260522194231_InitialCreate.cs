06590bc85be1757b7a2323e0adc2a2922f99b3dd
NRM <roman.nikitchuk123@gmail.com>
2026-05-22 21:53:50 +0200

    refactor: move migrations to persistence folder

--- Infrastructure/Persistence/Migrations/20260522194231_InitialCreate.cs
+++ Infrastructure/Persistence/Migrations/20260522194231_InitialCreate.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
--- Infrastructure/Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Infrastructure/Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
